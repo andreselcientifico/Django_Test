54d2959863914b19c5cc9e3e54f20fcba1b32d77
root <andresperezrodriguez28@gmail.com>
2023-05-01 20:36:25 -0500

    added database for questions

--- App/home/urls.py
+++ App/home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls, name='admin'),
     path("", views.home, name="home"),
+    path("index/", views.index, name="index"),
     path('users/', include('users.urls')),
 ]
--- App/home/views.py
+++ App/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,reverse
+from django.contrib import admin
 from django.http import HttpResponse
-
+from .models import Question
 
 def home(request):
     register_url = reverse('users:register')
@@ -11,3 +12,11 @@ def home(request):
     }
     path  = 'home/home.html'
     return render(request,path,button_link)
+
+def index(request):
+    latest_question_list = Question.objects.all()
+    path = 'home/index.html'
+    dic = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, path, dic)
